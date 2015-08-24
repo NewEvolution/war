f14507b8e9f06e3dd8f8bcc26c42d7e7a652b5fc
Ryan Tanay <ryantanay@newevolution.org>
2015-08-24 13:20:16 -0500

    Moved github auth modules into place

---
+++ javascripts/authentication.js
@@ -0,0 +1,20 @@
+define(function(require) {
+  var firebase = require("firebase");
+  var uid = require("uid");
+  var ref = new Firebase("https://sizzling-torch-4887.firebaseio.com");
+  var authData = ref.getAuth();
+  if(authData === null) {
+    ref.authWithOAuthPopup("github", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        uid.setUid(authData.uid);
+        require(["scaffold"], function() {});
+      }
+    },
+    {remember: "sessionOnly"});
+  } else {
+    uid.setUid(authData.uid);
+    require(["scaffold"], function() {});
+  }
+});
