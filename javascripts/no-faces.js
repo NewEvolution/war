b2ba8ec9ea657297d19691af7f33ae198bd11656
Ryan Tanay <ryantanay@newevolution.org>
2015-08-24 16:25:18 -0500

    Added in the defacer to turn face cards into a numerical value we can deal with.

--- javascripts/no-faces.js
+++ javascripts/no-faces.js
@@ -2,9 +2,22 @@ define(function(require) {
   var fight = require("fight");
   return {
     deface: function(bothCards) {
-      console.log("red card", bothCards.redCard);
-      console.log("blue card", bothCards.blueCard);
-
+      for(var key in bothCards) {
+        var cardValue = bothCards[key].cards[0].value;
+        if(cardValue === "ACE") {
+          cardValue = 14;
+        } else if(cardValue === "KING") {
+          cardValue = 13;
+        } else if(cardValue === "QUEEN") {
+          cardValue = 12;
+        } else if(cardValue === "JACK") {
+          cardValue = 11;
+        } else {
+          cardValue = parseInt(cardValue);
+        }
+        bothCards[key].cards[0].value = cardValue; 
+      }
+      fight.battle(bothCards.redCard, bothCards.blueCard);
     }
   };
 });
