0d6cb36ebcb7eea36a3b76ae08a9f58034ad671d
Ryan Tanay <ryantanay@newevolution.org>
2016-05-31 15:06:03 -0500

    Pulled firebase config settings out into separate module for references elsewhere

---
+++ javascripts/fireconf.js
@@ -0,0 +1,8 @@
+define(function(require) {
+  return {
+    apiKey: "AIzaSyDeLlaGb81ju_M4LuCuRdgGeAWich_kk2Y",
+    authDomain: "nss-card-war.firebaseapp.com",
+    databaseURL: "https://nss-card-war.firebaseio.com",
+    storageBucket: "nss-card-war.appspot.com"
+  };
+});
