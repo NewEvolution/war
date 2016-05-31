d2dd07603524b24f5432be15dab07804e8c2bcb0
Ryan Tanay <ryantanay@newevolution.org>
2016-05-31 16:28:26 -0500

    Pulled app initialization entirely into module

--- javascripts/fireconf.js
+++ javascripts/fireconf.js
@@ -1,8 +1,10 @@
 define(function(require) {
-  return {
+  var firebase = require("firebase");
+  firebase.initializeApp({
     apiKey: "AIzaSyDeLlaGb81ju_M4LuCuRdgGeAWich_kk2Y",
     authDomain: "nss-card-war.firebaseapp.com",
     databaseURL: "https://nss-card-war.firebaseio.com",
     storageBucket: "nss-card-war.appspot.com"
-  };
+  });
+  return firebase;
 });
