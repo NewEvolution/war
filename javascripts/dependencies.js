24d3610c9b7be78fbcefa08a19a63e31b6eb2948
Ryan Tanay <ryantanay@newevolution.org>
2016-06-01 11:00:25 -0500

    Added fireconf to initial dependencies

--- javascripts/dependencies.js
+++ javascripts/dependencies.js
@@ -1,5 +1,6 @@
 define(function(require) {
   var bootstrap = require("bootstrap");
   var firebase = require("firebase");
+  var fireconf = require("fireconf");
   var handlebars = require("hbs");
 });
