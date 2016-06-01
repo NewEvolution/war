8b2166337ebbb36bf4fa6507e98459130e7c92ca
Ryan Tanay <ryantanay@newevolution.org>
2016-06-01 11:31:16 -0500

    fireconf wasn't really required to be in the dependencies, the inclusion in the grunt requirejs task still had the unchanged capital Firebase. Fixed.

--- javascripts/dependencies.js
+++ javascripts/dependencies.js
@@ -1,6 +1,5 @@
 define(function(require) {
   var bootstrap = require("bootstrap");
   var firebase = require("firebase");
-  var fireconf = require("fireconf");
   var handlebars = require("hbs");
 });
